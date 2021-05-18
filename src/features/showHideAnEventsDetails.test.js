7d1eeee0e02a7f2e7061ba73a86f9aa4de181b18
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-05-18 15:58:51 -0600

    3.5 additions for task

---
+++ src/features/showHideAnEventsDetails.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { loadFeature, defineFeature } from "jest-cucumber";
+import { mount } from "enzyme";
+import { mockData } from "../mock-data";
+import App from "../App";
+
+const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
+
+defineFeature(feature, (test) => {
+  test("When user clicks Show Details button", ({ given, when, then }) => {
+    given("the main page is open", () => {});
+
+    let AppWrapper;
+    when("the user clicks Show Details", async () => {
+      AppWrapper = await mount(<App />);
+      await AppWrapper.update();
+      AppWrapper.find(".event .detailsButton").at(0).simulate("click");
+    });
+
+    then("the user should receive the details of an event", () => {
+      expect(AppWrapper.find(".event")).toHaveLength(mockData.length);
+    });
+  });
+
+  test("When user clicks Hide Details", ({ given, and, when, then }) => {
+    let AppWrapper;
+    given("user has clicked 'Show Details' button", async () => {
+      AppWrapper = await mount(<App />);
+      await AppWrapper.update();
+      AppWrapper.find(".event .detailsButton").at(0).simulate("click");
+    });
+
+    and("the details of event are showing", () => {
+      expect(AppWrapper.find(".event")).toHaveLength(mockData.length);
+    });
+
+    when("the user selects Hide Details", async () => {
+      AppWrapper = await mount(<App />);
+      await AppWrapper.update();
+      AppWrapper.find(".event .detailsButton").at(0).simulate("click");
+    });
+
+    then("the user should see the event without description", () => {
+      expect(AppWrapper.find(".event_details")).toHaveLength(0);
+    });
+  });
+});
