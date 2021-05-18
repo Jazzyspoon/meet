56c485cf9ca4b5cd3c06cdbc2042c5179a77e387
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-05-18 16:06:56 -0600

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -12,13 +12,9 @@ describe("show/hide an event details", () => {
       ignoreDefaultArgs: ["--disable-extensions"],
     });
     page = await browser.newPage();
-    await page.goto("http://localhost:3000");
+    await page.goto("http://localhost:3000/");
     await page.waitForSelector(".event");
   });
-
-  // afterAll(() => {
-  //   browser.close();
-  // });
 
   test("An event element is collapsed by default", async () => {
     const eventDetails = await page.$(".event .event__details");
@@ -30,6 +26,7 @@ describe("show/hide an event details", () => {
     const eventDetails = await page.$(".event .event__details");
     expect(eventDetails).toBeDefined();
   });
+
   test("User can collapse an event to hide its details", async () => {
     await page.click(".event .detailsButton");
     const eventDetails = await page.$(".event .event__details");
