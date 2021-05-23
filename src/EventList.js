7bd63de7286c2c4c6b752cf66463afe5bab0f6e3
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-05-23 09:57:44 -0600

    added two rows for event list

--- src/EventList.js
+++ src/EventList.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import Event from "./Event";
+import { Col, Row } from "react-bootstrap";
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
-      <ul className="EventList">
-        {events.map((event) => (
-          <li key={event.id}>
-            <Event event={event} />
-          </li>
-        ))}
+      <ul className="EventList event-rows">
+        <Row>
+          {events.map((event) => (
+            <Col lg={6} key={event.id}>
+              <Event event={event} />
+            </Col>
+          ))}
+        </Row>
       </ul>
     );
   }
